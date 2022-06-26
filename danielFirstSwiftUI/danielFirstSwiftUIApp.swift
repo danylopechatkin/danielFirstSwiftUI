dce49e9c2fbb43415ae650e96afe62d9ba441fd5
Danylo <pechatkin.work@gmail.com>
2022-06-26 23:33:49 +0200

    Initial Commit

---
+++ danielFirstSwiftUI/danielFirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  danielFirstSwiftUIApp.swift
+//  danielFirstSwiftUI
+//
+//  Created by Danylo Pechatkin on 26/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct danielFirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
