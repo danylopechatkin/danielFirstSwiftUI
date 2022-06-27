8a297e652ce33654b79697ec35100ed08a78acec
Danylo <pechatkin.work@gmail.com>
2022-06-28 00:15:08 +0200

    Setup ContentView

--- danielFirstSwiftUI/ContentView.swift
+++ danielFirstSwiftUI/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
-struct ContentView: View {
+
+
+let backgroundGradient = LinearGradient(
+    colors: [Color.red, Color.blue],
+    startPoint: .top, endPoint: .bottom)
+
+struct SignInView: View {
+    @State var name: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            backgroundGradient
+                .ignoresSafeArea()
+            VStack {
+                Text("Welcome")
+                    .font(.title)
+                HStack {
+                    TextField("Your name?", text: $name)
+                        .textFieldStyle(.roundedBorder)
+                    Button(action: {}, label: {
+                        Image(systemName: "arrow.right.square")
+                            .font(.title)
+                    })
+                }
+                .padding()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SignInView()
     }
 }
--- danielFirstSwiftUI/danielFirstSwiftUIApp.swift
+++ danielFirstSwiftUI/danielFirstSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct danielFirstSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SignInView()
         }
     }
 }
