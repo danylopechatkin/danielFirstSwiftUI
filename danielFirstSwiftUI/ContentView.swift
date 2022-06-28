62c5aed4be5757c1522faef4f40b5d4e31b67418
Danylo <pechatkin.work@gmail.com>
2022-06-28 21:30:03 +0200

    First SetupTest

--- danielFirstSwiftUI/ContentView.swift
+++ danielFirstSwiftUI/ContentView.swift
@@ -8,36 +8,46 @@
 import SwiftUI
 
 
-
 let backgroundGradient = LinearGradient(
-    colors: [Color.red, Color.blue],
+    colors: [Color.white, Color.pink, Color.gray, Color.yellow, Color.purple],
     startPoint: .top, endPoint: .bottom)
 
-struct SignInView: View {
-    @State var name: String = ""
+struct ContentView: View {
     var body: some View {
         ZStack {
             backgroundGradient
-                .ignoresSafeArea()
-            VStack {
-                Text("Welcome")
-                    .font(.title)
-                HStack {
-                    TextField("Your name?", text: $name)
-                        .textFieldStyle(.roundedBorder)
-                    Button(action: {}, label: {
-                        Image(systemName: "arrow.right.square")
-                            .font(.title)
-                    })
-                }
-                .padding()
+             .ignoresSafeArea()
+            
+         Rectangle()
+                .fill(.linearGradient(colors: [Color.pink, Color.red, Color.mint], startPoint: .topTrailing, endPoint: .bottomTrailing))
+                   .frame(height: 230)
+                   .cornerRadius(15)
+                   .padding(16)
+                   .shadow(radius: 15)
+            
+            
+            Button {
+                print("Button pressed")
+            } label: {
+                Text("Karta Iambrows")
+                    .padding(20)
+                    .font(.largeTitle)
+                    
+                    .foregroundStyle(.white)
             }
-        }
+            .contentShape(Rectangle())
+                
+            
+           
+            
+            
+            
     }
+} 
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        SignInView()
+        ContentView()
     }
 }
